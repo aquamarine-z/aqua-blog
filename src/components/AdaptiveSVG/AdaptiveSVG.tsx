f88a0db99bc8ae9d33bb66c9d2054cfa97ecf3b8
24180 <2418013844@qq.com>
2024-11-03 13:57:11 +0800

    feat:the maximum of AdaptiveSVG

--- src/components/AdaptiveSVG/AdaptiveSVG.tsx
+++ src/components/AdaptiveSVG/AdaptiveSVG.tsx
@@ -1,9 +1,14 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {ReactDOM, useEffect, useRef, useState} from "react";
+
 import styles from "./AdaptiveSVG.module.scss";
 // @ts-ignore
 import playImage from "/static/icon/MdiPlay.png"
 // @ts-ignore
 import maxImage from "/static/icon/SolarMaximizeLinear.png"
+import {createPortal, render} from "react-dom";
+import {createRoot} from "react-dom/client";
+import {style} from "d3";
+
 export interface AdaptiveSVGProps {
     onInitialize?: (svg?: SVGSVGElement, data?: any) => void,
     onUpdate?: (svg?: SVGSVGElement, data?: any) => void,
@@ -21,32 +26,60 @@ export interface AdaptiveSVGProps {
     onPlay?: (svg: SVGSVGElement, data?: any) => void,
 }
 
-function TitleBarButton({imageUrl,onClick}:{
-    imageUrl:string,
-    onClick:()=>void
-}){
-    return <img src={imageUrl} className={styles['title-bar']} onClick={onClick}/>
+function showMaximumImage(component: React.ReactNode) {
+
+    if (document.body.getElementsByClassName("modal").length != 0) {
+        return
+    }
+    const onClose = () => {
+        document.body.style.overflow = ""
+        document.body.removeChild(div)
+    }
+    const imageComponent = <div onClick={(event) => {
+        event.stopPropagation()
+        onClose()
+    }} className={styles["maximize-background"]}>
+        <div className={styles["maximize-container"]} onClick={(e) => {
+            e.stopPropagation()
+        }}>
+            {component}
+        </div>
+    </div>
+    const div = document.createElement("div")
+
+    document.body.appendChild(div)
+    document.body.style.overflow = "hidden"
+    div.setAttribute("class", "modal")
+    div.focus()
+    const root = createRoot(div)
+    root.render(imageComponent)
 }
-export function AdaptiveSVG({
-                                onInitialize = () => {
-                                },
-                                onUpdate = () => {
-                                },
-                                x = 0,
-                                y = 0,
-                                width = 1024,
-                                height = 768,
-                                data = undefined,
 
-                                canMaximize = true,
+function TitleBarButton({imageUrl, onClick}: {
+    imageUrl: string,
+    onClick: () => void
+}) {
+    return <img src={imageUrl} className={styles['title-bar-button']} onClick={onClick}/>
+}
 
-                                showInformation = true,
+export function AdaptiveSVG(props: AdaptiveSVGProps) {
+    const {
+        x = 0,
+        y = 0,
+        width = 1024,
+        height = 768,
+        data = undefined,
 
-                                playable = false,
-                                onPlay = () => {
-                                },
+        showInformation = true,
 
-                            }: AdaptiveSVGProps) {
+        canMaximize = true,
+
+        playable = false,
+        onInitialize = () => {
+        },
+        onUpdate=()=>{},
+        onPlay=()=>{},
+    } = props
     const svgRef = useRef<SVGSVGElement>()
     const startX = x ?? 0
     const startY = y ?? 0
@@ -57,14 +90,21 @@ export function AdaptiveSVG({
     const [viewBoxHeight, setViewBoxHeight] = useState(endY - startY)
     const [actualWidth, setActualWidth] = useState(0)
     const [actualHeight, setActualHeight] = useState(0)
-    const showTitleBar=canMaximize
+    const showTitleBar = canMaximize || playable
+    //console.log(showTitleBar)
 
+    const titleBar = showTitleBar ? <div className={styles["title-bar"]}>
+        {canMaximize && <TitleBarButton imageUrl={maxImage} onClick={() => {
+            showMaximumImage(
+                <AdaptiveSVG {...props} canMaximize={false}/>
+            )
+        }}/>}
+        {playable && <TitleBarButton imageUrl={playImage} onClick={() => {
+        }}/>}
+    </div> : <div style={{width: "100%", height: "20px"}}/>
     const svgElement = <div className={styles["svg-container"]}>
 
-        {showTitleBar ? <div className={styles["title-bar"]}>
-            <TitleBarButton imageUrl={playImage} onClick={()=>{}}></TitleBarButton>
-            <TitleBarButton imageUrl={maxImage} onClick={()=>{}}></TitleBarButton>
-        </div> : <div style={{width: "100%", height: "20px"}}/>}
+        {titleBar}
         <svg
             preserveAspectRatio={"xMidYMid meet"}
             className={styles["diagram"] + ` invert`}
@@ -91,7 +131,6 @@ export function AdaptiveSVG({
         }
     }, []);
     useEffect(() => {
-
         const viewBoxObserver = new MutationObserver(mutations => {
             mutations.forEach(mutation => {
                 if (mutation.type === 'attributes' && mutation.attributeName === 'viewBox') {
