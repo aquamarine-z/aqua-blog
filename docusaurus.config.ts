c3f39c9ac007360139818cfdb9c6351d2b95aa4e
24180 <2418013844@qq.com>
2024-12-18 20:39:17 +0800

    docs:Physics experiment review

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import fs from 'fs';
 import path from 'path';
 function customPostCssPlugin() {
@@ -93,6 +95,8 @@ const config: Config = {
             {
                 docs: {
                     sidebarPath: './sidebars.ts',
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     //editUrl:
@@ -110,7 +114,8 @@ const config: Config = {
                         'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
                     */
                     // Useful options to enforce blogging best practices
-
+                    remarkPlugins: [remarkMath],
+                    rehypePlugins: [rehypeKatex],
                     onInlineTags: 'warn',
                     onInlineAuthors: 'warn',
                     onUntruncatedBlogPosts: 'warn',
@@ -125,6 +130,15 @@ const config: Config = {
                 },
             } satisfies Preset.Options,
         ],
+    ],
+    stylesheets: [
+        {
+            href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+            type: 'text/css',
+            integrity:
+                'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+            crossorigin: 'anonymous',
+        },
     ],
     clientModules: [require.resolve('./src/clientModules/routeModules.ts')],
     themeConfig: {
